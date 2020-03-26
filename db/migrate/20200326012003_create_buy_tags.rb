2fcc42f916a785b536876224a860fde99973b30a
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-03-26 10:28:24 +0900

    rails db:migrate

--- db/migrate/20200326012003_create_buy_tags.rb
+++ db/migrate/20200326012003_create_buy_tags.rb
@@ -1,8 +1,8 @@
 class CreateBuyTags < ActiveRecord::Migration[5.2]
   def change
     create_table :buy_tags do |t|
-      t.references :buy, foreign_key: true
-      t.references :tag, foreign_key: true
+      t.references :buy, foreign_key: true, index: true
+      t.references :tag, foreign_key: true, index: true
 
       t.timestamps
     end
