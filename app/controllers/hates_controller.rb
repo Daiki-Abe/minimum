d80d245b027f9b4f5cfce2fc3de7f48ec986ddd4
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-03-29 17:12:02 +0900

    Add def create and destroy

--- app/controllers/hates_controller.rb
+++ app/controllers/hates_controller.rb
@@ -1,2 +1,20 @@
 class HatesController < ApplicationController
+  before_action :set_buy, only: [:create, :destroy]
+
+  def create
+    @hate = current_user.hates.create(buy_id: params[:buy_id])
+    @buys = Buy.all
+  end
+
+  def destroy
+    hate = current_user.hates.create(buy_id: params[:buy_id])
+    hate.destroy
+    @buys = Buy.all
+  end
+
+  private
+
+  def set_buy
+    @buy = Buy.find(params[:buy_id])
+  end
 end
