33cb278ea56f48d3bf51eb0923b67567f9c9ae02
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-03-29 12:17:53 +0900

    Add create method in controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
 class CommentsController < ApplicationController
+  def create
+    Comment.create(comment_params)
+    redirect_to buy_path(comment.buy.id)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, buy_id: params[:buy_id])
+  end
 end
