cec3a9914aa581cf0bf7f05fab78dcd0ee0d8e51
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-03-29 18:38:26 +0900

    Add gem 'jquery-rails'

--- app/controllers/hates_controller.rb
+++ app/controllers/hates_controller.rb
@@ -2,16 +2,14 @@ class HatesController < ApplicationController
   before_action :set_buy, only: [:create, :destroy]
 
   def create
-    @buys = Buy.all
     @hate = current_user.hates.create(buy_id: params[:buy_id])
-    redirect_to buy_path
+    @buys = Buy.all
   end
 
   def destroy
-    @buys = Buy.all
     hate = current_user.hates.find_by(buy_id: params[:buy_id])
     hate.destroy
-    redirect_to buy_path
+    @buys = Buy.all
   end
 
   private
