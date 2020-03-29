ee9b8112782f2f0571dd3b26548b1fc2be9a10bd
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-03-29 12:06:58 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
     collection do
       get 'search'
     end
+    resources :comments, only: [:create]
   end
+
   resources :users, only: [:show]
-  resources :comments, only: [:create]
 end
