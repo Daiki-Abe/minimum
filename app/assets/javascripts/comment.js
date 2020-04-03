54a423c5283efb0bcc6e38b559b9916371a7a4fd
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-04-03 10:22:46 +0900

    Add view of ajax comment

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,7 +1,18 @@
 $(function(){
-  $('buy-comment__send-form').on('submit', function(e){
+  function buildHTML(comment){
+    var html = `<div class = buy-comment__list>
+                  <div class = buy-comment__list-writter>
+                    ${comment.user_name}:
+                  </div>
+                  <div class = buy-comment__list-comment>
+                    ${comment.text}
+                  </div>
+                </div>`
+    return html;
+  }
+  $('#buy-comment__send-form').on('submit', function(e){
     e.preventDefault();
-    var formData = new formData(this);
+    var formData = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
       url: url,
@@ -11,5 +22,14 @@ $(function(){
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.ajax').append(html);
+      $('.buy-comment__send-text').val('');
+      $('.buy-comment__send-submit').prop('disabled', false);
+    })
+    .fail(function(){
+      alert('error');
+    })
   })
-})
+});
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.create(comment_params)
     respond_to do |format|
-      format.html {redirect_to "/buys/#{comment.buy.id}"}
+      format.html {redirect_to "/buys/#{@comment.buy.id}"}
       format.json
     end
   end
