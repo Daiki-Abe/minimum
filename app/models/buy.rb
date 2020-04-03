850261a5aededf8dba22bda785faabc747bb5aa4
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-04-03 10:24:32 +0900

    Update buy.rb

--- app/models/buy.rb
+++ app/models/buy.rb
@@ -2,7 +2,7 @@ class Buy < ApplicationRecord
   belongs_to :user
   has_many :buy_tags, dependent: :destroy
   has_many :tags, through: :buy_tags
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :hates, dependent: :destroy
 
   with_options presence: true do
