45416f0ef3601fcbda18c1e9e07f9d8f6fcbfc63
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-04-05 08:51:48 +0900

    Add move_to_index in comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :move_to_index, only: [:create]
   def create
     @comment = Comment.create(comment_params)
     respond_to do |format|
@@ -11,4 +12,9 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, buy_id: params[:buy_id])
   end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in? 
+  end
+
 end
