99610e278109ecbb480c399593de5958b6dbca4f
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-04-05 10:16:45 +0900

    Edit hate count

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    buys = @user.buys
+    buy_id = []
+    buys.each do |buy|
+      buy_id << buy.id
+    end
+    @hate_count = Hate.where(buy_id: buy_id).count
   end
 
   def mybuy
