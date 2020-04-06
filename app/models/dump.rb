401c41979b0882b2efcf6ea2924418b3c4009a39
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-04-07 08:15:25 +0900

    Add association and validates

--- app/models/dump.rb
+++ app/models/dump.rb
@@ -1,2 +1,12 @@
 class Dump < ApplicationRecord
+  belongs_to :user
+
+  with_options presence: true do
+    validates :goods
+    validates :price
+    validates :description
+  end
+
+  validates :price, numericality: {only_integer: true, message: "は半角数字で入力してください"}
+  
 end
