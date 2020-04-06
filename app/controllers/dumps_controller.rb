722edf8827c34346cd0ee53d062ff0ddb0446ce1
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-04-07 08:31:00 +0900

    Add new-action and create-action

--- app/controllers/dumps_controller.rb
+++ app/controllers/dumps_controller.rb
@@ -1,2 +1,16 @@
 class DumpsController < ApplicationController
+  def new
+    @dump = Dump.new
+  end
+
+  def create
+    @dump = Dump.new(dump_params)
+    @dump.save
+    render :new unless @dump.save
+  end
+
+  private
+
+  def dump_params
+    params.require(:dump).permit(:goods, :price, :image, :description).merge(user_id: current_user.id)
 end
