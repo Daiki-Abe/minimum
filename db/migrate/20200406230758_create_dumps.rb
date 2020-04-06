5b6eb3d95a3825d436be4e3d15f923bccef55500
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-04-07 08:13:13 +0900

    rails db:migrate

--- db/migrate/20200406230758_create_dumps.rb
+++ db/migrate/20200406230758_create_dumps.rb
@@ -1,6 +1,11 @@
 class CreateDumps < ActiveRecord::Migration[5.2]
   def change
     create_table :dumps do |t|
+      t.string :goods, null: false
+      t.string :price, null: false
+      t.string :image
+      t.text :description, null: false
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
