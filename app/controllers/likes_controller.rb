636dc164a2d3e35ede7f93bcf5bc2184a6e3e8e6
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-04-07 16:02:44 +0900

    Update likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,23 @@
 class LikesController < ApplicationController
+  before_action :set_dump, :move_to_index, only: [:create, :destroy]
+
+  def create
+    @like = Like.create(user_id: current_user.id, dump_id: @dump.id)
+  end
+
+  def destroy
+    @like = Like.find_by(user_id: current_user.id, dump_id: @dump.id)
+    @like.destroy
+  end
+
+  private
+
+  def set_dump
+    @dump = Dump.find(params[:dump_id])
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in? 
+  end
+
 end
