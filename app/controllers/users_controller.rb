2bf982a29cd4ffcb2bee74c33e01b6b4fb22bbae
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-04-08 11:47:55 +0900

    Add mydump in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,11 @@ class UsersController < ApplicationController
     @dump_count = Like.where(dump_id: dump_id).count
   end
 
+  def mydump
+    @user = User.find(params[:id])
+    @dumps = params[:tag_id].present? ? Tag.find(params[:tag_id]).dumps.includes(:user) : Dump.includes(:user)
+  end
+
   def mybuy
     @user = User.find(params[:id])
     @buys = params[:tag_id].present? ? Tag.find(params[:tag_id]).buys.includes(:user) : Buy.includes(:user)
