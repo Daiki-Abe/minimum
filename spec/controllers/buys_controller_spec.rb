523e228611b87bd3112a26fa3e218a4fd0eea48a
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-04-11 15:09:43 +0900

    Add login user in buys_controller_spec.rb

--- spec/controllers/buys_controller_spec.rb
+++ spec/controllers/buys_controller_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 describe BuysController, type: :controller do
+  let(:user) {create(:user)}
+  let(:buy) {create(:buy)}
+
   describe 'GET #index' do
     it "@buysに正しい値が入っていること" do
       buys = create_list(:buy, 3)
@@ -15,23 +18,54 @@ describe BuysController, type: :controller do
   end
 
   describe 'GET #new' do
-    it "new.html.hamlに遷移すること" do
-      get :new
-      expect(response).to render_template :new
+    context 'ログインしている場合' do
+      before do
+        login user
+        get :new
+      end
+
+      it "new.html.hamlに遷移すること" do
+        expect(response).to render_template :new
+      end
+    end
+
+    context 'ログインしていない場合' do
+      before do
+        get :new
+      end
+
+      it 'indexページにリダイレクトすること' do
+        expect(response).to redirect_to(buys_path)
+      end
     end
   end
 
+  
+
   describe 'GET #edit' do
-    it "@buyに正しい値が入っていること" do
-      buy = create(:buy)
-      get :edit, params: {id: buy}
-      expect(assigns(:buy)).to eq buy
+    context 'ログインしてる場合' do
+      before do
+        login user
+        get :edit, params: {id: buy}
+      end
+
+      it "@buyに正しい値が入っていること" do
+        expect(assigns(:buy)).to eq buy
+      end
+      
+      it "edit.html.hamlに遷移すること" do
+        expect(response).to render_template :edit
+      end
     end
 
-    it "edit.html.hamlに遷移すること" do
-      buy = create(:buy)
-      get :edit, params: {id: buy}
-      expect(response).to render_template :edit
+    context 'ログインしていない場合' do
+      before do
+        get :edit, params: {id: buy}
+      end
+
+      it 'indexページにリダイレクトすること' do
+        expect(response).to redirect_to(buys_path)
+      end
     end
   end
 end
