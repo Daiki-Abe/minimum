3086fa2313372e07262ca090f292696e89f232d8
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-04-12 22:40:44 +0900

    Update buys_controller_spec.rb

--- spec/controllers/buys_controller_spec.rb
+++ spec/controllers/buys_controller_spec.rb
@@ -2,16 +2,17 @@ require 'rails_helper'
 
 describe BuysController, type: :controller do
   let(:user) {create(:user)}
+  let(:tag) {create(:tag)}
   let(:buy) {create(:buy)}
 
   describe 'GET #index' do
-    it "@buysに正しい値が入っていること" do
+    it '@buysに正しい値が入っていること' do
       buys = create_list(:buy, 3)
       get :index
       expect(assigns(:buys)).to match(buys.sort{ |a, b| b.created_at <=> a.created_at } )
     end
 
-    it "index.haml.hamlに遷移すること" do
+    it 'indexページに遷移すること' do
       get :index
       expect(response).to render_template :index
     end
@@ -24,7 +25,7 @@ describe BuysController, type: :controller do
         get :new
       end
 
-      it "new.html.hamlに遷移すること" do
+      it 'newページに遷移すること' do
         expect(response).to render_template :new
       end
     end
@@ -40,7 +41,59 @@ describe BuysController, type: :controller do
     end
   end
 
-  
+  describe 'POST #create' do
+    let(:params) { { buy: attributes_for(:buy, user_id: user.id, buy_tags_attributes: [tag_id: tag.id]) } }
+
+    context 'ログインしてる場合' do
+      before do
+        login user
+      end
+
+      context '保存に成功した場合' do
+        subject {
+          post :create,
+          params: params
+        }
+
+        it 'buyを保存すること' do
+          expect{subject}.to change(Buy, :count).by(1)
+        end
+
+        it 'createページに遷移すること' do
+          subject
+          expect(response.status).to eq(200)
+        end
+      end
+
+      context '保存に失敗した場合' do
+        it 'newページに遷移されること' do
+          post :create, params: { buy: attributes_for(:buy, user_id: "") } 
+          expect(response).to render_template :new
+        end
+      end
+    end
+
+    context 'ログインしていない場合' do
+      it 'indexページにリダイレクトすること' do
+        post :create, params: params
+        expect(response).to redirect_to(buys_path)
+      end
+    end
+  end
+
+  describe 'GET #show' do
+    before do
+      get :show, params: {id: buy}
+    end
+
+    it '@buyに正しい値が入っていること' do
+      expect(assigns(:buy)).to eq buy
+    end
+
+    it 'showページに遷移されること' do
+      expect(response.status).to eq(200)
+    end
+  end
 
   describe 'GET #edit' do
     context 'ログインしてる場合' do
@@ -53,17 +106,78 @@ describe BuysController, type: :controller do
         expect(assigns(:buy)).to eq buy
       end
       
-      it "edit.html.hamlに遷移すること" do
+      it "editページに遷移すること" do
         expect(response).to render_template :edit
       end
     end
 
     context 'ログインしていない場合' do
-      before do
+      it 'indexページにリダイレクトすること' do
         get :edit, params: {id: buy}
+        expect(response).to redirect_to(buys_path)
+      end
+    end
+  end
+
+  describe "PATCH #update" do
+  let(:params) { { id: buy, buy: attributes_for(:buy, goods: "hogehoge", user_id: user.id, buy_tags_attributes: [tag_id: tag.id]) } }
+
+    context 'ログインしている場合' do
+      before do
+        login user
+        patch :update, params: params
       end
 
-      it 'indexページにリダイレクトすること' do
+      context '更新に成功した場合' do
+        it '@buyに正しい値が入っていること' do
+          expect(assigns(:buy)).to eq buy
+        end
+
+        it '更新処理がされること' do
+          buy.reload
+          expect(buy.goods).to eq("hogehoge") 
+        end
+
+        it "updateページに遷移すること" do
+          expect(response.status).to eq(200)
+        end
+      end
+
+      context '更新に失敗した場合' do
+        it 'editページに遷移されること' do
+          patch :update, params: params
+          expect(response).to render_template :update
+        end
+      end
+    end
+
+    context 'ログインしていない場合' do
+      it 'indexページに遷移されること' do
+        patch :update, params: params
+        expect(response).to redirect_to(buys_path)
+      end
+    end
+  end
+
+  describe 'DLETE #destroy' do
+    context 'ログインしている場合' do
+      before do
+        login user
+      end
+
+      it '削除されること' do
+        buy = create(:buy)
+        expect{delete :destroy, params: {id: buy}}.to change(Buy, :count).by(-1)
+      end
+
+      it 'destroyページに遷移されること' do
+        expect(delete :destroy, params: {id: buy}).to render_template :destroy
+      end
+    end
+
+    context 'ログインしていない場合' do
+      it 'indexページに遷移すること' do
+        delete :destroy, params: {id: buy}
         expect(response).to redirect_to(buys_path)
       end
     end
