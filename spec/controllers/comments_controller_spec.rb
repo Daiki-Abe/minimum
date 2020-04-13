72feed323cb073de0b50dc8420c7fe1b2c0bb917
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-04-13 21:03:47 +0900

    Create comments_controller_spec.rb

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe CommentsController, type: :controller do
+  let(:user) {create(:user)}
+  let(:buy) {create(:buy)}
+  let(:comment) {create(:comment)}
+
+  describe 'POST #create', js: true do
+    let(:params) { { buy_id: buy, comment: attributes_for(:comment, user_id: user.id, buy_id: buy.id)  } }
+    subject { post :create, params: params }
+
+    it 'commentを保存すること' do
+      login user
+      expect{subject}.to change(Comment, :count).by(1)
+    end
+  end
+end
